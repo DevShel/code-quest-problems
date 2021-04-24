93a9f51beec3086e4bb4160d2c0a56546f165097
Sheldon <55363444+DevShel@users.noreply.github.com>
2021-04-24 10:04:23 -0400

    commit

--- Phonebook.py
+++ Phonebook.py
@@ -3,16 +3,24 @@ import math
 import string
 # Practice Scenario:https://wma.contest.lmcodequestacademy.com/team/problems/8/text
 
-def phone_book_output(inputText):
-    str(inputText)
-    if (inputText[14]=="E"): #this is input for parentheses
-        print("(" + inputText[0:3] + ") " + inputText[3:6] + "-" + inputText[6:10])
-    if (inputText[14]=="H"): #this is input for dashes
-        print(inputText[0:3] + "-" + inputText[3:6] + "-" + inputText[6:10])
-    if (inputText[14]=="I"): #this is input for periods
-        print(inputText[0:3] + "." + inputText[3:6] + "." + inputText[6:10])
+testCases = int(input())
+inputArray = []
 
-inputText = input()
-phone_book_output(inputText)
+for i in range(testCases):
+    inputText = input()
+    str(inputText)
+    inputArray.append(inputText)
+
+for i in range(testCases):
+    tempCase = inputArray[i]
+    if (tempCase[14]=="E"): #this is input for parentheses
+        print("(" + tempCase[0:3] + ") " + tempCase[3:6] + "-" + tempCase[6:10])
+    if (tempCase[14]=="H"): #this is input for dashes
+        print(tempCase[0:3] + "-" + tempCase[3:6] + "-" + tempCase[6:10])
+    if (tempCase[14]=="I"): #this is input for periods
+        print(tempCase[0:3] + "." + tempCase[3:6] + "." + tempCase[6:10])
+    i = i+1
+ 
+
 
 
